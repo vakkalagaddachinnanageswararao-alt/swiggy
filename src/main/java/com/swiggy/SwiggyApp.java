3a32c818b811b586dc4174b82959b669f78d330f
vakkalagaddachinnanageswararao-alt <vakkalagaddachinnanageswararao@gmail.com>
2026-02-27 15:28:45 +0530

    Refactor HTML response construction in SwiggyApp

--- src/main/java/com/swiggy/SwiggyApp.java
+++ src/main/java/com/swiggy/SwiggyApp.java
@@ -1,4 +1,4 @@
-package com.swiggy;
+ package com.swiggy;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
@@ -12,32 +12,27 @@ public class SwiggyApp {
 
         HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
 
-        // Home Page
         server.createContext("/", (HttpExchange exchange) -> {
-            String response = """
-                    <html>
-                    <head>
-                        <title>Swiggy App</title>
-                    </head>
-                    <body>
-                        <h1>Welcome to Swiggy</h1>
-                        <h2>Menu</h2>
-                        <ul>
-                            <li>Chicken Biryani - ₹250 <a href="/order?item=Biryani">Order</a></li>
-                            <li>Pizza - ₹300 <a href="/order?item=Pizza">Order</a></li>
-                            <li>Burger - ₹120 <a href="/order?item=Burger">Order</a></li>
-                        </ul>
-                    </body>
-                    </html>
-                    """;
+            String response =
+                    "<html>" +
+                    "<head><title>Swiggy App</title></head>" +
+                    "<body>" +
+                    "<h1>Welcome to Swiggy</h1>" +
+                    "<h2>Menu</h2>" +
+                    "<ul>" +
+                    "<li>Chicken Biryani - ₹250 <a href='/order?item=Biryani'>Order</a></li>" +
+                    "<li>Pizza - ₹300 <a href='/order?item=Pizza'>Order</a></li>" +
+                    "<li>Burger - ₹120 <a href='/order?item=Burger'>Order</a></li>" +
+                    "</ul>" +
+                    "</body>" +
+                    "</html>";
 
-            exchange.sendResponseHeaders(200, response.length());
+            exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         });
 
-        // Order Page
         server.createContext("/order", (HttpExchange exchange) -> {
             String query = exchange.getRequestURI().getQuery();
             String item = "Item";
@@ -46,17 +41,16 @@ public class SwiggyApp {
                 item = query.substring(5);
             }
 
-            String response = """
-                    <html>
-                    <body>
-                        <h1>Order Successful!</h1>
-                        <p>You ordered: """ + item + """</p>
-                        <a href="/">Go Back</a>
-                    </body>
-                    </html>
-                    """;
+            String response =
+                    "<html>" +
+                    "<body>" +
+                    "<h1>Order Successful!</h1>" +
+                    "<p>You ordered: " + item + "</p>" +
+                    "<a href='/'>Go Back</a>" +
+                    "</body>" +
+                    "</html>";
 
-            exchange.sendResponseHeaders(200, response.length());
+            exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();

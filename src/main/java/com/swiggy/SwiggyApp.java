9300f640ac9b3a0389a3c389db7cb5a28de726c9
vakkalagaddachinnanageswararao-alt <vakkalagaddachinnanageswararao@gmail.com>
2026-02-27 14:32:44 +0530

    Update SwiggyApp.java

--- src/main/java/com/swiggy/SwiggyApp.java
+++ src/main/java/com/swiggy/SwiggyApp.java
@@ -1,64 +1,24 @@
 package com.swiggy;
 
-import java.util.*;
+import com.sun.net.httpserver.HttpServer;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class SwiggyApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Scanner sc = new Scanner(System.in);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        List<Food> menu = new ArrayList<>();
-        List<Order> orders = new ArrayList<>();
+        server.createContext("/", exchange -> {
+            String response = "Swiggy Application Running Successfully!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
 
-        menu.add(new Food(1, "Chicken Biryani", 250));
-        menu.add(new Food(2, "Veg Fried Rice", 150));
-        menu.add(new Food(3, "Pizza", 300));
-        menu.add(new Food(4, "Burger", 120));
-
-        int choice;
-
-        do {
-            System.out.println("\n===== Swiggy Menu =====");
-            for (Food food : menu) {
-                food.display();
-            }
-
-            System.out.println("Enter Food ID (0 to Exit): ");
-            choice = sc.nextInt();
-
-            if (choice == 0) break;
-
-            Food selected = null;
-            for (Food food : menu) {
-                if (food.getId() == choice) {
-                    selected = food;
-                    break;
-                }
-            }
-
-            if (selected != null) {
-                System.out.println("Enter Quantity: ");
-                int qty = sc.nextInt();
-                orders.add(new Order(selected, qty));
-                System.out.println("Added to Cart!");
-            } else {
-                System.out.println("Invalid ID!");
-            }
-
-        } while (true);
-
-        double total = 0;
-        System.out.println("\n===== Order Summary =====");
-
-        for (Order order : orders) {
-            order.displayOrder();
-            total += order.calculateTotal();
-        }
-
-        System.out.println("Total Bill: ₹" + total);
-        System.out.println("Thank You!");
-
-        sc.close();
+        server.start();
+        System.out.println("Server started on port 8080");
     }
 }

7e6a76ec169c92106264ac253b651499b723c661
vakkalagaddachinnanageswararao-alt <vakkalagaddachinnanageswararao@gmail.com>
2026-02-27 16:23:48 +0530

    Change servlet imports from jakarta to javax

--- src/main/java/com/swiggy/SwiggyServlet.java
+++ src/main/java/com/swiggy/SwiggyServlet.java
@@ -1,7 +1,7 @@
 package com.swiggy;
 
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 

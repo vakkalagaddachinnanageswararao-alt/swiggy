373fde3345fb0b99aeb3ac593dd7255fdec03cb4
vakkalagaddachinnanageswararao-alt <vakkalagaddachinnanageswararao@gmail.com>
2026-02-27 14:42:49 +0530

    Update SwiggyApp.java

--- src/main/java/com/swiggy/SwiggyApp.java
+++ src/main/java/com/swiggy/SwiggyApp.java
@@ -8,7 +8,7 @@ public class SwiggyApp {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(9090), 0);
 
         server.createContext("/", exchange -> {
             String response = "Swiggy Application Running Successfully!";
